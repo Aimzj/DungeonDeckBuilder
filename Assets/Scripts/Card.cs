5742f5b62e86de3de6cb0ec30a54c32c8262553b
Aimzj <aimeejhandley@gmail.com>
2018-08-17 09:55:27 +0200

    Floaty card effect!

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Card", menuName ="Card")]
+public class NewBehaviourScript : ScriptableObject {
+    public string CardName;
+    public string Description;
+
+    public Sprite Artwork;
+
+    public int Cost;
+    public int Attack;
+    public int Defense;
+}
