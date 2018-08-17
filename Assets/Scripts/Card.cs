3978bce9140db86b8faa32273f80f0482f7a1bbe
Aimzj <aimeejhandley@gmail.com>
2018-08-17 11:22:07 +0200

    Basic Card Blueprint and Excel Spreadsheet
    - added some fonts
    - added the card excel spread sheet
    - card blueprint

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -7,9 +7,36 @@ public class NewBehaviourScript : ScriptableObject {
     public string CardName;
     public string Description;
 
+    //BOOLEANS
+    public bool isDiscardEffect;
+    public bool isBurnEffect;
+    public bool isFree = false;
+    public bool hasSigil = false;
+    public bool useHighestVal_Attack = false;
+
+    //ART
     public Sprite Artwork;
 
-    public int Cost;
+    //COSTS
+    public int DiscardCost;
+    public int BurnCost;
     public int Attack;
     public int Defense;
+
+    //DISCARD EFFECTS
+    public int numDiscard_Deck;
+    public int numDiscard_Hand;
+    public int numDrawCards;
+    public int numDiscardDraw;
+    public int numPoisonsPlace;
+    public int numPickupFromDiscard;
+
+    //BURN EFFECTS
+    public int numBurn_Deck;
+    public int numBurn_Hand;
+    public bool canUnsear = false;
+
+    public enum type {Player, Enemy};
+
+    public type CardType = type.Player;
 }
